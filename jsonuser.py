62bb70a1264be8c1f32d32f97957ff018e08e5d8
Vraj Patel <Vraj@MacBook-Pro-7.local>
2022-11-15 17:47:10 -0800

    fixed all bugs and tested user input datawork

--- jsonuser.py
+++ jsonuser.py
@@ -20,11 +20,11 @@ def user_creation(userInfo):
             userInfo.update({"ID": user1})
             password1 = input("Enter your password: \n")
             userInfo.update({"password": password1})
-            interest1 = input("Out of Music,Art,and Sports Please select your 1ST favorite activity: \n")
+            interest1 = input("Out of Music, Entertainment,and Sports Please select your 1ST favorite activity: \n")
             userInfo.update({"interest1": interest1})
-            interest2 = input("Out of Music,Art,and Sports Please select your 2ND favorite activity: \n")
+            interest2 = input("Out of Music,Entertainment,and Sports Please select your 2ND favorite activity: \n")
             userInfo.update({"interest2": interest2})
-            interest3 = input("Out of Music,Art,and Sports Please select your 3RD favorite activity: \n")
+            interest3 = input("Out of Music,Entertainment,and Sports Please select your 3RD favorite activity: \n")
             userInfo.update({"interest3": interest3})
             write_to_users(userInfo)
             break
@@ -33,16 +33,11 @@ def user_creation(userInfo):
 def write_to_users(userInfo):
     try:
         print(userInfo)
-        allInfo = dict
-        allInfo.update(userInfo)
-        print(allInfo)
         with open("users.json", 'a') as outfile:
             json.dump(userInfo, outfile, indent=2)
     except IOError:
         print("I/O error")
  
-
-
 userInfo = dict({"email": "Email", "ID": "User", "password": "Password", "interest1": "INT1", "interest2": "INT2", "interest3": "INT3" })
 for i in range(2):  
     user_creation(userInfo)
