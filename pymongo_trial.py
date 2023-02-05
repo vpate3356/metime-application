314715568f45696c1e9aef8155ce41642554a8d9
Vraj Patel <Vraj@MacBook-Pro-8.local>
2023-02-04 18:50:09 -0800

    Connected to MongoDB Atlas succesfully with uri

--- pymongo_trial.py
+++ pymongo_trial.py
@@ -1,2 +1,22 @@
-include pymongo
+import pymongo
 
+ 
+# Provide the mongodb atlas url to connect python to mongodb using pymongo
+#CONNECTION_STRING =  "mongodb+srv://vrajp00:bapa3356@metimeapp.kjhpdea.mongodb.net/?retryWrites=true&w=majority"
+                   
+
+# Connect to the MongoDB Atlas cluster
+uri = "mongodb+srv://vrajp00:bapa3356@metimeapp.kjhpdea.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri)
+
+# Get the sample database
+db = client.test
+
+# Get the sample collection
+#collection = db["sample_collection"]
+
+# Check if the connection was successful
+#f collection:
+print("Connected to MongoDB Atlas successfully!")
+#else:
+#   print("Connection to MongoDB Atlas failed.")
