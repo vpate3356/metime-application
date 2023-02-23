1e3c0f7ef29dbd7cc4953f04de4c5723f9718552
vboxuser <ethanwan5@gmail.com>
2023-02-22 20:37:14 -0800

    Modified GPlacesAPI.py and jsonuser.py * GPlacesAPI.py now accepts user inputted address before displaying open art galleries nearby * jsonuser.py now asks and stores user address

--- GPlacesAPI.py
+++ GPlacesAPI.py
@@ -1,10 +1,13 @@
 # in terminal
 # pip install googlemaps
 # pip install prettyprint
+# pip install geopy
 
 import googlemaps
 import pprint
 import time 
+
+from geopy.geocoders import Nominatim
 
 #define API key   -- my project key 
 API_KEY= "AIzaSyBdqRy6zIcskbk59IvAO11cTzDVfI8itaQ"
@@ -12,8 +15,23 @@ API_KEY= "AIzaSyBdqRy6zIcskbk59IvAO11cTzDVfI8itaQ"
 #Define Client
 gmaps = googlemaps.Client(key = API_KEY)            #aunthetic yourself 
 
+# takes in user input for address and gets latitude/longitude, see below for what user should input
+# United States, pass in the address components in the format {house number} {street} {city} {state} {zip}.
+# For outside USA: {house number} {street} {city} {state} {zip} or follow local address formatting example below
+# 123 Main St Swindon SN2 2DQ (UK)
+# Fraunhoferstraße 6 80469 München Bavaria (Germany)
+userInput = input() # example input: 3637 Canyon Crest Dr Riverside, CA 92507
+
+geolocator = Nominatim(user_agent="my_request")
+loc = geolocator.geocode(userInput)
+print(loc.address)
+print((loc.latitude, loc.longitude))
+loc2 = str(loc.latitude)
+loc2 += ','
+loc2 += str(loc.longitude)
+
 #define our search 
-places_result = gmaps.places_nearby(location = '33.979120,-117.341070', radius = 10000, open_now = True, type = 'art_gallery' ) 
+places_result = gmaps.places_nearby(location = loc2, radius = 10000, open_now = True, type = 'art_gallery' ) 
 #grandmarc with .2 raduis  --> raduis is measured meters 
 #  can ADD all kinds of filters from here :https://developers.google.com/maps/documentation/places/web-service/search-nearby
 # only one type parameter from = https://developers.google.com/maps/documentation/places/web-service/supported_types
