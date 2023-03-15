16f7bebd6848eed9cc4d1e5e12c65919ae763840
Vraj Patel <Vraj@MacBook-Pro-8.local>
2023-03-15 15:00:13 -0700

    Got it so we show Mongodb data on web app

---
+++ metime-app/pages/api/places.tsx
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse} from 'next'
+import clientPromise from './auth/lib/mongodb'
+import { PlacesNear} from '@/pages/placesclose'
+
+
+type Return = {
+    places: PlacesNear[]
+};
+
+export const getPlaces = async () =>{
+    const mongoClient = await clientPromise;
+    const data = await mongoClient
+        .db('UserData')
+        .collection('PlacesNearby')
+        .find({})
+        .limit(5)
+        .map(user =>({...user, _id: user._id.toString()}))
+        .toArray();
+    return JSON.parse(JSON.stringify(data));
+}
+
+export default async (req: NextApiRequest, res:NextApiResponse<Return>) => {
+    const data = await getPlaces();
+    res.status(200).json({places : data});
+};
